a13c63ce8692d8053ece57a30d4ac281f6b5a595
John Michael D. Caol-olan <129568396+miicooo@users.noreply.github.com>
2023-08-20 14:54:23 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,21 @@ function showSlides() {
    slides[slideIndex - 1].style.display = "block";
    setTimeout(showSlides, 5000); // Change slide every 5 seconds
 }
+<script>
+    document.getElementById('search-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const searchTerm = document.getElementById('search-input').value.toLowerCase();
+        const movieLinks = document.querySelectorAll('.category a');
+        
+        movieLinks.forEach(link => {
+            const title = link.querySelector('h2').textContent.toLowerCase();
+            const description = link.querySelector('p').textContent.toLowerCase();
+            
+            if (title.includes(searchTerm) || description.includes(searchTerm)) {
+                link.style.display = 'block';
+            } else {
+                link.style.display = 'none';
+            }
+        });
+    });
+</script>
