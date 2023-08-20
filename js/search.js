f2d7b14e562fc5684a3d6dba9b728a1efdd9236d
John Michael D. Caol-olan <129568396+miicooo@users.noreply.github.com>
2023-08-20 16:04:38 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,5 +1,4 @@
 document.getElementById('search-form').addEventListener('submit', function(event) {
-    alert('Form submitted'); // Test alert
     event.preventDefault();
     const searchTerm = document.getElementById('search-input').value.toLowerCase();
     const movieLinks = document.querySelectorAll('.category a');
