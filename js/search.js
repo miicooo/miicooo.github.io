0b18580e22dee6f688f803fab0bee126b0df96e8
John Michael D. Caol-olan <129568396+miicooo@users.noreply.github.com>
2023-08-20 15:31:45 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,29 +1,8 @@
-// search.js
-console.log('Search Term:', searchTerm);
+// js/search.js
 document.getElementById('search-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const searchTerm = document.getElementById('search-input').value.toLowerCase();
-    const movieLinks = document.querySelectorAll('.category a');
-    const searchResultsContainer = document.getElementById('search-results');
-    let matchingMovies = [];
-
-    movieLinks.forEach(link => {
-        const title = link.querySelector('h2').textContent.toLowerCase();
-        const description = link.querySelector('p').textContent.toLowerCase();
-        const movieName = link.getAttribute('data-movie-name').toLowerCase();
-
-        if (title.includes(searchTerm) || description.includes(searchTerm) || movieName.includes(searchTerm)) {
-            matchingMovies.push(link.cloneNode(true));
-        }
-    });
-
-    // Display search results
-    if (matchingMovies.length > 0) {
-        searchResultsContainer.innerHTML = '';
-        matchingMovies.forEach(movie => {
-            searchResultsContainer.appendChild(movie);
-        });
-    } else {
-        searchResultsContainer.innerHTML = '<p>No matching movies found.</p>';
-    }
+    
+    // Redirect to search-results.html with the search term as a query parameter
+    window.location.href = `search-results.html?q=${searchTerm}`;
 });
